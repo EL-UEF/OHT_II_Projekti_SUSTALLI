f39f49ee6578eb89ca676482a33d90d5adbade6b
EL-UEF <166162702+EL-UEF@users.noreply.github.com>
2026-02-22 13:53:42 +0200

    Add files via upload

---
+++ PaivanTiedot.java
@@ -0,0 +1,23 @@
+
+
+package com.example.new2;
+
+
+class PaivanTiedot {
+    double jate;
+    double energia;
+    double vesi;
+    double lanta;
+    double kuivike;
+    double auto;
+
+    public PaivanTiedot(double jate, double energia, double vesi,
+                        double lanta, double kuivike, double auto) {
+        this.jate = jate;
+        this.energia = energia;
+        this.vesi = vesi;
+        this.lanta = lanta;
+        this.kuivike = kuivike;
+        this.auto = auto;
+    }
+}
